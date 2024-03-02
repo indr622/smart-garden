d0cac6b37bedd2f7207e8f68c195e97209d4aa4a
indra <indrbasuki1@gmail.com>
2024-03-02 23:41:47 +0700

    bootstrap

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+   <div>
+      <h1>Welcome to the homepage</h1>
+      <p>Click the button below to login</p>
+
+      <button class="btn btn-primary">Login</button>
+   </div>
+</template>
